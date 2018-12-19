d106b77f289c759f598ec5cf71f5dc2e11210290
Larson Carter <larsoncarter@blazermail.net>
2018-12-19 10:04:28 -0600

    Update program.java

--- CLIENT/program.java
+++ CLIENT/program.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
-public class Program {
+public class program {
 
   public static void main(String[] args) {
 
       System.out.println("What is your username?");
 			
-			System.out.println("What is your password?");
-
-      String input = "";
-
       Scanner input1 = new Scanner(System.in);
 
       String username = input1.nextLine();
 
+      System.out.println("What is your password?");
+
+      Scanner input2 = new Scanner(System.in);
+
+      String password = input2.nextLine();
 
   }
 
