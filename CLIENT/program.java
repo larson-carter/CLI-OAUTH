8de20a519301e485d2f2cc3b358f70c83400c0fc
Larson Carter <larsoncarter@blazermail.net>
2018-12-19 09:11:01 -0600

    Update program.java

--- CLIENT/program.java
+++ CLIENT/program.java
@@ -1,11 +1,20 @@
-	Program {
+import java.util.Scanner;
 
-		public static void main() {
+public class Program {
 
-			System.out.println("What is your username?");
+  public static void main(String[] args) {
+
+      System.out.println("What is your username?");
 			
 			System.out.println("What is your password?");
 
-		}
+      String input = "";
 
-	}
+      Scanner input1 = new Scanner(System.in);
+
+      String username = input1.nextLine();
+
+
+  }
+
+}
